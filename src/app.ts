fa7945f6191d5e22fbe2733708815a4630160969
IDHAR001 <943967062@qq.com>
2023-10-18 23:18:16 +0800

    format

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
-import * as express from 'express';
-import * as http from 'http';
-import * as dotenv from 'dotenv';
-
+import * as express from "express";
+import * as http from "http";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello world!');
+app.get("/", function (req, res) {
+  res.send("Hello world!");
 });
 
 app.listen(3000);
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
-import server from './app';
+import server from "./app";
 
 if (require.main === module) {
-    (async () => {
-        const port = parseInt(process.env.PORT || '8080', 10);
-        const expressServer = server.listen(port, () => {
-            console.log('listening the port :>>', port);
-
-        });
+  (async () => {
+    const port = parseInt(process.env.PORT || "8080", 10);
+    const expressServer = server.listen(port, () => {
+      console.log("listening the port :>>", port);
+    });
     expressServer.setTimeout(60);
-    })();
+  })();
 }
